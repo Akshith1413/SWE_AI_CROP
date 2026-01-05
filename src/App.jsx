3a8299addaf81bf76ae29c217fac41c6649b5bfd
Ravula Akshith <ravulaakshith1@gmail.com>
2026-01-05 23:14:33 +0530

    hi

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+import CAM from './components/CropDiagnosisApp';
+function App() {
+  return (
+    <>
+      <CAM />
+    </>
+  )
+}
+
+export default App
