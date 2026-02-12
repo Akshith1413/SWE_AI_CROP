8df7e02405a8ccf2b9a96a8c2cb3d669129a52e2
Ravula Akshith <ravulaakshith1@gmail.com>
2026-02-12 12:11:52 +0530

    Fixed Calender issues

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import { offlineSync } from './offlineSync';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 export const api = {
     auth: {
